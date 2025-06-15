ede9981367bc3cf53b7933f183e6c1f2a216ffc6
Eder Sena <eder.sena@live.com>
2025-06-15 18:25:32 -0300

    Initial commit

---
+++ src/test/java/br/Cardatabase/CardatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.Cardatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
