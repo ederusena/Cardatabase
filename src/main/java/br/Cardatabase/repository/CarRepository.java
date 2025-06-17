46659ef5f86ef3677b6307beeb4c1a00897617af
Eder Sena <eder.sena@live.com>
2025-06-17 19:46:24 -0300

    Adicionar anotação RepositoryRestResource ao CarRepository para definir o caminho da API

--- src/main/java/br/Cardatabase/repository/CarRepository.java
+++ src/main/java/br/Cardatabase/repository/CarRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import br.Cardatabase.domain.Car;
-
+@RepositoryRestResource(path="vehicles")
 public interface CarRepository extends CrudRepository<Car, Long> {
     // Fetch cars by brand
     //List<Car> findByBrand(String brand);
